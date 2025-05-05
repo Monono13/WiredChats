da2375e82daffe6516886d01bdbe2b853fc85e34
Monono13 <luisalfredocividanes@gmail.com>
2025-05-05 12:04:35 -0400

    update

--- src-tauri/build_all.sh
+++ src-tauri/build_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "🔧 Compilando para Linux..."
-cargo tauri build
-
 echo "🪟 Compilando para Windows..."
 cargo tauri build --runner cargo-xwin --target x86_64-pc-windows-msvc
+
+echo "🔧 Compilando para Linux..."
+cargo tauri build
 
 echo "✅ Builds completadas"
